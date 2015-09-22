397226c41cf960473daccf81a0abe9bd15018fd2
Eliu Florez <eflorez@socialh4ck.com>
2015-09-22 12:10:16 -0430

    Routes - Credits

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['news'] = 'news';
+$route['credits'] = 'news';
 
 $route['settings/update'] = 'settings/index';
 $route['settings'] = 'settings';
